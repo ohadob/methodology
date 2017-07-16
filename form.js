320c3f20d457eda7789c8673ebea4a3761ee41b1
Ohad Obershtein <oobershtein@paloaltonetworks.com>
2017-07-16 20:32:56 +0300

    Improved styling

--- form.js
+++ form.js
@@ -10,14 +10,16 @@ $(document).ready(function(){
 	}
 
 	$("div#form1").append(			// Creating Form div and adding <h2> and <p> paragraph tag in it.
-		$("<h3/>").text("Project Form"),
-		$("<p/>").text("Please fill it in the name of science!"),
+		$("<h3/>").text("Thank you for taking part in my project!"),
+		$("<p/>").text("Please describe a software project that you were deeply involved in"),
  
         $("<form/>",{action:'#', method:'#'}).append(   // Create <form> tag and appending in html div form1.
-		$("<input/>", {type:'text', id:'vname', name:'name', placeholder:'Your Name'}),  // Creating input element with attribute
+		$("<input/>", {type:'text', id:'vname', name:'name', placeholder:'Your Name'}), // Creating input element with attribute
+    $("<br/>"), $("<br/>"),
 		$("<input/>", {type:'text', id:'vemail', name:'email', placeholder:'Your Email'}),
+    $("<br/>"), $("<br/>"),
 		$("<textarea/>", {rows:'5px', cols:'27px', type:'text', id:'vmsg', name:'msg', placeholder:'Message'}),
-		$("<br/>"),
+		$("<br/>"), $("<br/>"),
 		$("<input/>", {type:'submit', id:'submit', value:'Submit'})
 		).submit(saveData)
 	  )
