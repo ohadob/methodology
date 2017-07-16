e33ac1c00c9a59a1153ef9a44956a0d90547d372
Ohad Obershtein <oobershtein@paloaltonetworks.com>
2017-07-16 23:55:25 +0300

    more styling and object serialization of form.

--- form.js
+++ form.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-	function saveData(a, b) {
+  $.fn.serializeObject = function()
+  {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || null);
+      } else {
+        o[this.name] = this.value || null;
+      }
+    });
+    return o;
+  };
 
-		alert(JSON.stringify($('form').serializeArray()));
-    var data = { name: $("#vname").val() };
+	function saveData(a, b) {
+		var data = $('form').serializeObject();
     alert(JSON.stringify(data));
     $.post("/save", data, function(result) {
       alert(result ? "Thank you!" : "An error occured :( ");
@@ -10,19 +25,23 @@ $(document).ready(function(){
 	}
 
 	$("div#form1").append(			// Creating Form div and adding <h2> and <p> paragraph tag in it.
-		$("<h3/>").text("Thank you for taking part in my project!"),
-		$("<p/>").text("Please describe a software project that you were deeply involved in"),
- 
-        $("<form/>",{action:'#', method:'#'}).append(   // Create <form> tag and appending in html div form1.
-		$("<input/>", {type:'text', id:'vname', name:'name', placeholder:'Your Name'}), // Creating input element with attribute
-    $("<br/>"), $("<br/>"),
-		$("<input/>", {type:'text', id:'vemail', name:'email', placeholder:'Your Email'}),
-    $("<br/>"), $("<br/>"),
-		$("<textarea/>", {rows:'5px', cols:'27px', type:'text', id:'vmsg', name:'msg', placeholder:'Message'}),
-		$("<br/>"), $("<br/>"),
-		$("<input/>", {type:'submit', id:'submit', value:'Submit'})
-		).submit(saveData)
-	  )
+		$("<h3/>").text("Thank you for participating in my project!"),
+		$("<p/>").text("Please describe a software project you were involved in:"),
+    $("<br/>"),
+    $("<form/>",{action:'#', method:'#', style: "width: 400px"}).append(   // Create <form> tag and appending in html div form1.
+			$("<fieldset/>").append(
+				$("<legend/>", {text: "Personal Info"}),
+				$("<input/>", {type:'text', name:'name', placeholder:'Your Name'}), // Creating input element with attribute
+				$("<br/>"), $("<br/>"),
+				$("<input/>", {type:'text', name:'email', placeholder:'Your Email'})),
+			$("<br/>"), $("<br/>"),
+
+			$("<fieldset/>").append(
+        $("<legend/>", {text: "Organization"}),
+        $("<input/>", {type:'text', name:'orgSize', placeholder:'size (number of people)'}), // Creating input element with attribute
+				$("<br/>"), $("<br/>")),
+			$("<input/>", {type:'submit', id:'submit', value:'Submit'})
+		).submit(saveData))
 
 });
 
