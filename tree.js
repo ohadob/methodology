c3e9309a70473a6bf0c192ceb361a45ab2f99827
Ohad Obershtein <ohadob@gmail.com>
2018-01-28 20:02:04 +0200

    CMONNN

--- tree.js
+++ tree.js
@@ -30,25 +30,27 @@ function buildTree(training_data, class_name, features) {
     return dt;
 }
 
+var mapProject = p => ({
+    success: (Number(p.mesCustomer) + Number(p.mesContent) + Number(p.mesBudget) + Number(p.mesSchedule)) >= minSuccess,
+    projMethod: methodEnum[p.projMethod],
+    orgSize: sizeEnum[p.orgSize],
+    orgFlexibility: Number(p.orgFlexibility),
+    orgResources: Number(p.orgResources),
+    projDuration: Number(p.projDuration),
+    projEffort: Number(p.projEffort),
+    projRisk: Number(p.projRisk),
+    projQuality: Number(p.projQuality),
+    projReliability: Number(p.projReliability),
+    projExperience: Number(p.projExperience),
+    projLife : Number(p.projLife)
+});
+
 var dt = null;
 var req = {};
 db.queryAll(req, () => {
     var class_name = 'success';
     var projects = req.results;
-    var training = projects.map(p => ({
-        success: (Number(p.mesCustomer) + Number(p.mesContent) + Number(p.mesBudget) + Number(p.mesSchedule)) >= minSuccess,
-        projMethod: methodEnum[p.projMethod],
-        orgSize: sizeEnum[p.orgSize],
-        orgFlexibility: p.orgFlexibility,
-        orgResources: p.orgResources,
-        projDuration: p.projDuration,
-        projEffort: p.projEffort,
-        projRisk: p.projRisk,
-        projQuality: p.projQuality,
-        projReliability: p.projReliability,
-        projExperience: p.projExperience,
-        projLife : p.projLife
-    }));
+    var training = projects.map(mapProject);
 
     var features = 
         ["projMethod",
@@ -67,11 +69,28 @@ db.queryAll(req, () => {
     console.log('class_name: ', class_name);
     console.log('features: ', JSON.stringify(features));
 
-    buildTree(training, class_name, features);
+    dt = buildTree(training, class_name, features);
+
+
+    var mulp = dt.predict({ 'Organization Size': 2,
+    orgFlexibility: 5,
+    orgResources: 5,
+    projDuration: 12,
+    projEffort: 10,
+    projRisk: 1,
+    projQuality: 5,
+    projReliability: 4,
+    projExperience: 5,
+    projLife: 2,
+    projMethod: 4,
+    projMethodOther: '' })
+
+    console.log('mulp: ', mulp);
 });
 
 function predict(data) {
-    return dt.predict(data);
+    const project = mapProject(data);
+    return dt.predict(project);
 }
 
 module.exports = { predict };
